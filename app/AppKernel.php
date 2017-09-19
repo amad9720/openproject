8450cdad283b3d5b643f5505a3c8b582b9f53fa6
Amadou Ly <amly212782@gmail.com>
2017-09-19 11:41:18 +0200

    New Bundle for Managing Meals

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Als\PlateformBundle\AlsPlateformBundle(),
             new Sup\TestBundle\SupTestBundle(),
+            new Hlt\ManageurBundle\HltManageurBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
